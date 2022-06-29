de588f21ad481387c8db1279cf526fa77f88c2e1
iamhi <inpactt@gmail.com>
2022-06-29 22:42:02 +0200

    Initial

---
+++ Dockerfile
@@ -0,0 +1,37 @@
+#
+# Build stage
+#
+FROM maven:3.8.5-openjdk-18-slim AS build
+
+# Set the working directory to /app
+WORKDIR /app
+
+# copy the pom.xml file to download dependencies
+COPY pom.xml ./
+
+COPY ./config ./config
+COPY ./core ./core
+COPY ./in ./in
+COPY ./in-adapter ./in-adapter
+COPY ./out ./out
+COPY ./out-adapter ./out-adapter
+COPY ./service ./service
+
+# Compile the application.
+RUN mvn -Dmaven.test.skip=true package -P docker && cp service/target/messagebox-docker.jar app.jar
+
+#
+# Package stage
+#
+FROM eclipse-temurin:18.0.1_10-jre-jammy
+
+# set deployment directory
+WORKDIR /app
+
+COPY --from=build /app/app.jar ./app.jar
+
+ENV VAULT_TOKEN "null"
+
+EXPOSE 80
+
+CMD ["java", "-jar", "/app/app.jar"]
---
+++ service/src/main/java/com/github/iamhi/hizone/messagebox/service/MessageboxApplication.java
@@ -0,0 +1,16 @@
+package com.github.iamhi.hizone.messagebox.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+
+@SpringBootApplication(scanBasePackages = {"com.github.iamhi.hizone.messagebox"})
+@ConfigurationPropertiesScan("com.github.iamhi.hizone.messagebox.config")
+@EnableReactiveMongoRepositories("com.github.iamhi.hizone.messagebox.out")
+public class MessageboxApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MessageboxApplication.class, args);
+    }
+
+}
